67ea299d40dbfe53bcbe69239ea0bafcfa0c300e
Bruno Grassano <bgrassano@fi.uba.ar>
2022-10-26 14:17:58 -0300

    Add basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,36 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import { Rentals } from './views/Rentals';
 import AuthPage from "./views/auth/AuthPage";
+import NavBar from './components/Navbar';
+import { HomePage } from './views/HomePage';
+
+
+const NotLoggedInWeb = () => {
+  return <>
+        <Routes>
+            <Route path="/" element={<AuthPage />} />
+            <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+  </>
+}
+
+const LoggedInWeb = () => {
+  return <>
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/rentals" element={<Rentals />} />
+        </Routes>
+  </>
+}
 
 function App() {
+  const loggedIn = true // check if there is a token
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<AuthPage />} />
-            <Route path="/rentals" element={<Rentals />} />
-            <Route path="/auth" element={<AuthPage />} />
-        </Routes>
+        {!loggedIn && <NotLoggedInWeb />}
+        {loggedIn && <LoggedInWeb />}
       </BrowserRouter>
     </div>
   );
