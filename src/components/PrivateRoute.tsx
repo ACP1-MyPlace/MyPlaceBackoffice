8d16bae3de13e5d7098f4db9becd7cfd7d460114
Maxi Romero <mromerov@fi.uba.ar>
2022-10-29 13:56:43 -0300

    Se persiste sesión del usuario a través del Local Storage

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import { userStorage } from "../userSession/userStorage";
+
+export const PrivateRoute = () => {
+    let location = useLocation();
+    let auth : boolean = userStorage.isLogged();
+
+    return auth ? <Outlet /> : <Navigate to="/auth" state={{ from: location }} replace />;
+}
